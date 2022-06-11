df9cf0f08fddfa7e28d93c04dcde469f59f40b78
Johnson Ubaezuonu <jaycodist@gmail.com>
2022-06-11 09:25:10 +0100

    fixed meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/icons/favicon.png" />
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta
             name="description"
             content="Fresh flowers vendors. Delivery throughout Nigeria"
           />
