a9d5f060f7893518cbf2e6d6d52b607f3812bcbb
Taiwobello <bellotaiwolateef1999@gmail.com>
2022-06-17 21:51:25 +0100

    add flexibility support

--- components/FlowerCard/FlowerCard.tsx
+++ components/FlowerCard/FlowerCard.tsx
@@ -1,30 +1,39 @@
 import React, { FunctionComponent } from "react";
-import Product from "../../utils/types/Product";
 import Button from "../Button/Button";
 import styles from "./FlowerCard.module.scss";
 
 interface IFlowerCardProps {
-  flower: Product;
+  buttonText?: string;
+  image: string;
+  price?: number;
+  name: string;
+  details: string;
 }
 
 const FlowerCard: FunctionComponent<IFlowerCardProps> = props => {
-  const { flower } = props;
+  const { buttonText, image, price, name, details } = props;
   return (
     <div className={`${styles["flower-card"]} center`}>
       <img
         className={styles["flower-image"]}
-        src={flower.images[0].src}
+        src={image}
         alt="featured flower"
       />
       <div className={styles.detail}>
-        <p className="bold">{flower.name}</p>
-        <p className="smaller text-secondary">{flower.name}</p>
-        <div className="flex between margin-top spaced">
-          <div>
-            <p className="smaller text-secondary">From</p>
-            <p className="bold">{flower.price}</p>
-          </div>
-          <Button clasName={`${styles["buy-btn"]}`}>Buy Now</Button>
+        <p className="bold">{name}</p>
+        <p className="smaller text-secondary">{details}</p>
+        <div
+          className={`flex margin-top spaced ${price ? "between" : "center"}`}
+        >
+          {price && (
+            <div>
+              <p className="smaller text-secondary">From</p>
+              <p className="bold">{price}</p>
+            </div>
+          )}
+          <Button clasName={`${styles["buy-btn"]}`}>
+            {buttonText ? buttonText : "Buy Now"}
+          </Button>
         </div>
       </div>
     </div>
