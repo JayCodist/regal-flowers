ec4083d97ec5fbd3537bf7cf82f0f14a88317f54
Johnson Ubaezuonu <jaycodist@gmail.com>
2022-06-18 10:30:30 +0100

    started with landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import { FunctionComponent } from "react";
+import styles from "./index.module.scss";
 
 const LandingPage: FunctionComponent = () => {
-  return <h1>Landings!!</h1>;
+  return (
+    <section className="page-content">
+      <div className={[styles["hero-bg"], "hero-bg"].join(" ")}>
+        <div className="hero-content flex column center center-align">
+          <h1 className={styles.title}>
+            Same Day Flower Delivery <br />
+            In Lagos & Abuja, Nigeria.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LandingPage;
