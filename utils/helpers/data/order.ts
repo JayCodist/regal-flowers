ba021bc5361e6835930103c5c6322a33acd29ba4
Taiwobello <bellotaiwolateef1999@gmail.com>
2022-11-07 17:09:17 +0100

    integrate order

--- utils/helpers/data/order.ts
+++ utils/helpers/data/order.ts
@@ -1,4 +1,4 @@
-import { Order } from "../../types/Order";
+import { CreateOrder, Order } from "../../types/Order";
 import RequestResponse from "../../types/RequestResponse";
 import { restAPIInstance } from "../rest-api-config";
 
@@ -7,6 +7,49 @@ export const getOrder: (
 ) => Promise<RequestResponse<Order>> = async id => {
   try {
     const response = await restAPIInstance.get(`/v1/firebase/order/${id}`);
+    return {
+      error: false,
+      data: response.data as Order
+    };
+  } catch (err) {
+    return {
+      error: true,
+      message: (err as Error).message,
+      data: null
+    };
+  }
+};
+
+export const createOrder: (
+  order: CreateOrder
+) => Promise<RequestResponse<Order>> = async order => {
+  try {
+    const response = await restAPIInstance.post(
+      `/v1/firebase/order/create`,
+      order
+    );
+    return {
+      error: false,
+      data: response.data as Order
+    };
+  } catch (err) {
+    return {
+      error: true,
+      message: (err as Error).message,
+      data: null
+    };
+  }
+};
+
+export const updateOrder: (
+  id: string,
+  order: CreateOrder
+) => Promise<RequestResponse<Order>> = async (id, order) => {
+  try {
+    const response = await restAPIInstance.put(
+      `/v1/firebase/order/update/${id}`,
+      order
+    );
     return {
       error: false,
       data: response.data as Order
--- utils/types/Order.ts
+++ utils/types/Order.ts
@@ -1,13 +1,43 @@
+import { Dayjs } from "dayjs";
+
 interface OrderItem {
-  id: string;
+  id?: string;
   name: string;
   quantity: number;
 }
 
 export interface Order {
-  orderProducts: OrderItem[];
-  paymentStatus: string;
-  orderID: number;
-  deliveryStatus: string;
-  fullOrderId: string;
+  orderProducts?: OrderItem[];
+  paymentStatus?: string;
+  orderID?: number;
+  deliveryStatus?: string;
+  fullOrderId?: string;
+  id: string;
+  cost: number;
+}
+
+export interface CreateOrder {
+  orderProducts?: OrderItem[];
+  paymentStatus?: string;
+  cost?: number;
+  client?: {
+    name?: string;
+    email?: string;
+    phone?: string;
+    password?: string;
+  };
+  recipient?: {
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    phoneAlt?: string;
+  };
+  deliveryState?: string;
+  pickUpLocation?: string;
+  deliveryDate?: Dayjs | string;
+  purpose?: string;
+  message?: string;
+  additionalInfo?: string;
+  pickUpState?: string;
 }
