aa58fd01d12c08dbd167037b90a90c01bd903247
Taiwobello <bellotaiwolateef1999@gmail.com>
2022-11-08 02:30:04 +0100

    fix styles

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -132,11 +132,23 @@ const Header: FunctionComponent = () => {
                 cartItems.length && setShowCart(!showCart);
               }}
             >
-              <img
-                alt="cart"
-                src="/icons/cart.svg"
+              <svg
+                width="29"
+                height="24"
+                viewBox="0 0 24 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
                 className={styles["control-icon"]}
-              />
+              >
+                <path
+                  d="M1 1H5L7.68 14.39C7.77144 14.8504 8.02191 15.264 8.38755 15.5583C8.75318 15.8526 9.2107 16.009 9.68 16H19.4C19.8693 16.009 20.3268 15.8526 20.6925 15.5583C21.0581 15.264 21.3086 14.8504 21.4 14.39L23 6H6"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+
               <span>Cart ({cartItems.length})</span>
             </button>
             <button className="flex column center-align">
