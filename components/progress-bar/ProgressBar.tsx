27d2ec4e2a21a395bc1129bb88a52b204e4f9791
Taiwobello <bellotaiwolateef1999@gmail.com>
2023-09-14 13:05:47 +0100

    fix progress bar display

--- components/progress-bar/ProgressBar.tsx
+++ components/progress-bar/ProgressBar.tsx
@@ -4,13 +4,15 @@ import styles from "./ProgressBar.module.scss";
 
 const ProgressBar = () => {
   const [width, setWidth] = useState(0);
+  const [routeChanged, setRouteChanged] = useState(false);
 
   useEffect(() => {
     const handleRouteChange = () => {
+      setRouteChanged(false);
       setWidth(0);
       // Simulate progress (for demonstration purposes)
-      const interval = setTimeout(() => {
-        if (width < 90) {
+      const interval = setInterval(() => {
+        if (width < 100) {
           setWidth(prevWidth => prevWidth + 10);
         } else {
           clearInterval(interval);
@@ -20,7 +22,7 @@ const ProgressBar = () => {
 
     // Subscribe to route changes
     Router.events.on("routeChangeStart", handleRouteChange);
-    Router.events.on("routeChangeComplete", () => setWidth(100));
+    Router.events.on("routeChangeComplete", () => setRouteChanged(true));
 
     return () => {
       Router.events.off("routeChangeStart", handleRouteChange);
@@ -29,7 +31,9 @@ const ProgressBar = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [Router]);
 
-  if (width === 0 || width >= 100) {
+  const showProgressBar = routeChanged ? 0 : width;
+
+  if (showProgressBar === 0) {
     return null;
   }
 
